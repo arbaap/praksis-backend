5103c153d68ede3fc8fc52e5784e46f8f2c0e85f
arbaap <arbaadhipamungkas@gmail.com>
2023-06-05 19:36:10 +0700

    halo

--- api/convertimage.js
+++ api/convertimage.js
@@ -5,8 +5,6 @@ const fs = require("fs");
 
 const router = express.Router();
 
-const upload = multer({ dest: "uploads_convert_image/" });
-
 router.get("/", async (req, res) => {
   try {
     res.json({
@@ -19,63 +17,65 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.use("/uploads_convert_image", express.static("uploads_convert_image"));
+// const upload = multer({ dest: "uploads_convert_image/" });
 
-router.post("/api/upload", upload.single("image"), (req, res) => {
-  const imagePath = req.file.path;
-  const processedImagePath = `processed_${req.file.originalname}`;
-  const fileExtension = req.file.originalname.split(".").pop().toLowerCase();
+// router.use("/uploads_convert_image", express.static("uploads_convert_image"));
 
-  let format = "jpeg";
+// router.post("/api/upload", upload.single("image"), (req, res) => {
+//   const imagePath = req.file.path;
+//   const processedImagePath = `processed_${req.file.originalname}`;
+//   const fileExtension = req.file.originalname.split(".").pop().toLowerCase();
 
-  if (fileExtension === "png") {
-    format = "png";
-  }
+//   let format = "jpeg";
 
-  sharp(imagePath)
-    .resize({
-      width: 800,
-      height: 600,
-      fit: sharp.fit.inside,
-      withoutEnlargement: true,
-    })
-    .toColorspace("srgb")
-    .jpeg({ quality: 50 })
-    .toFormat(format)
-    .toFile(`uploads_convert_image/${processedImagePath}`, (error, info) => {
-      fs.unlink(imagePath, (unlinkError) => {
-        if (unlinkError) {
-          console.error("Error deleting temporary file:", unlinkError);
-        }
-      });
+//   if (fileExtension === "png") {
+//     format = "png";
+//   }
 
-      if (error) {
-        console.error("Error processing image:", error);
-        res.status(500).json({ error: "Image processing failed" });
-      } else {
-        const processedImageUrl = `/uploads_convert_image/${processedImagePath}`;
-        const imageDetails = {
-          processedImageSize: info.size,
-          processedImageWidth: info.width,
-          processedImageHeight: info.height,
-        };
-        res.json({ processedImageUrl, imageDetails });
-      }
-    });
-});
+//   sharp(imagePath)
+//     .resize({
+//       width: 800,
+//       height: 600,
+//       fit: sharp.fit.inside,
+//       withoutEnlargement: true,
+//     })
+//     .toColorspace("srgb")
+//     .jpeg({ quality: 50 })
+//     .toFormat(format)
+//     .toFile(`uploads_convert_image/${processedImagePath}`, (error, info) => {
+//       fs.unlink(imagePath, (unlinkError) => {
+//         if (unlinkError) {
+//           console.error("Error deleting temporary file:", unlinkError);
+//         }
+//       });
 
-router.get("/api/download/:imageName", (req, res) => {
-  const imageName = req.params.imageName;
-  const imagePath = `uploads_convert_image/${imageName}`;
+//       if (error) {
+//         console.error("Error processing image:", error);
+//         res.status(500).json({ error: "Image processing failed" });
+//       } else {
+//         const processedImageUrl = `/uploads_convert_image/${processedImagePath}`;
+//         const imageDetails = {
+//           processedImageSize: info.size,
+//           processedImageWidth: info.width,
+//           processedImageHeight: info.height,
+//         };
+//         res.json({ processedImageUrl, imageDetails });
+//       }
+//     });
+// });
 
-  res.setHeader("Content-Type", "image/jpeg");
+// router.get("/api/download/:imageName", (req, res) => {
+//   const imageName = req.params.imageName;
+//   const imagePath = `uploads_convert_image/${imageName}`;
 
-  res.download(imagePath, (error) => {
-    if (error) {
-      console.error("Error downloading image:", error);
-      res.status(500).send("Error downloading image");
-    }
-  });
-});
+//   res.setHeader("Content-Type", "image/jpeg");
+
+//   res.download(imagePath, (error) => {
+//     if (error) {
+//       console.error("Error downloading image:", error);
+//       res.status(500).send("Error downloading image");
+//     }
+//   });
+// });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const convertaudio = require("./api/convertaudio.js");
-// const convertimage = require("./api/convertimage.js");
-// const mergeaudio = require("./api/mergeaudio.js");
+const convertaudio = require("./api/convertaudio.js");
+const convertimage = require("./api/convertimage.js");
+const mergeaudio = require("./api/mergeaudio.js");
 
 app.use(express.json());
 app.use(cors());
@@ -15,9 +15,9 @@ app.get("/", async (req, res, next) => {
   });
 });
 
-// app.use("/api/convertaudio.js", convertaudio);
-// app.use("/api/convertimage.js", convertimage);
-// app.use("/api/mergeaudio.js", mergeaudio);
+app.use("/api/convertaudio.js", convertaudio);
+app.use("/api/convertimage.js", convertimage);
+app.use("/api/mergeaudio.js", mergeaudio);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Server is running on port", port));
