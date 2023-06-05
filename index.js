83b78a15f9f3f25bc78f2b6ac9213fceb666befa
arbaap <arbaadhipamungkas@gmail.com>
2023-06-05 19:06:16 +0700

    push

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.get("/", async (req, res, next) => {
   });
 });
 
-app.use("/audio", convertAudio);
-app.use("/image", convertImage);
-app.use("/maudio", mergeAudio);
+app.use("/convertAudio", convertAudio);
+app.use("/convertImage", convertImage);
+app.use("/mergeAudio", mergeAudio);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Server is running on port", port));
