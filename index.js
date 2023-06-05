d14e82968377e7cb0dea949303b37d6cbcefc860
arbaap <arbaadhipamungkas@gmail.com>
2023-06-05 19:29:15 +0700

    pushsss

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const convertaudio = require ("./api/convertaudio.js")
+const convertimage = require ("./api/convertimage.js")
+const mergeaudio = require ("./api/mergeaudio.js")
 
 app.use(express.json());
 app.use(cors());
@@ -11,6 +14,11 @@ app.get("/", async (req, res, next) => {
     message: "The app is working properly!",
   });
 });
+
+app.use("/api/convertaudio.js",convertaudio)
+app.use("/api/convertimage.js",convertimage)
+app.use("/api/mergeaudio.js",mergeaudio)
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Server is running on port", port));
