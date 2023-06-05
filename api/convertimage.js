6946fa290e6d69cbce46e4e6684f320899364279
arbaap <arbaadhipamungkas@gmail.com>
2023-06-06 05:47:28 +0700

    image

--- api/convertimage.js
+++ api/convertimage.js
@@ -12,14 +12,7 @@ router.get("/", async (req, res, next) => {
   });
 });
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads_convert_image/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
 
