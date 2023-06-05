6262b27dd711cc47df03c344d7190b11d2cb7286
arbaap <arbaadhipamungkas@gmail.com>
2023-06-05 18:52:41 +0700

    pp

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const convertAudio = require("./convertaudio");
-const convertImage = require("./convertimage");
-const mergeAudio = require("./mergeaudio");
 
 app.use(express.json());
 app.use(cors());
@@ -14,10 +11,6 @@ app.get("/", async (req, res, next) => {
     message: "The app is working properly!",
   });
 });
-
-app.use("/", convertAudio);
-app.use("/", convertImage);
-app.use("/", mergeAudio);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Server is running on port", port));
